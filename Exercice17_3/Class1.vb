f2f9ce47e9e2350a8d375d856a6b6dbeccfdd293
unknown <le_gall@220056slocal>
2025-01-28 14:24:34 +0100

    Add C#

---
+++ Exercice17_3/Class1.vb
@@ -0,0 +1,39 @@
+﻿Public Class Pile
+    Private Const MAX As Integer = 100
+    Private tabPile(MAX - 1) As String
+    Private positionlibre As Integer
+    Public Sub New()
+        positionlibre = 0
+    End Sub
+    Public Function Empiler(ByVal valeur As String) As Boolean
+        If positionlibre < MAX Then
+            tabPile(positionlibre) = valeur
+            positionlibre += 1
+            Return True
+        Else
+            Return False
+        End If
+    End Function
+    Public Function Depiler() As String
+        If positionlibre > 0 Then
+            positionlibre -= 1
+            Return tabPile(positionlibre)
+        Else
+            Return Nothing
+        End If
+    End Function
+    Public Function EstVide() As Boolean
+        Return positionlibre = 0
+    End Function
+    Public Function NombreDElements() As Integer
+        Return positionlibre
+    End Function
+    Public Overrides Function ToString() As String
+        Dim contenu As String = "-----" & vbCrLf
+        For i As Integer = positionlibre - 1 To 0 Step -1
+            contenu &= tabPile(i) & vbCrLf
+        Next
+        contenu &= "-----"
+        Return contenu
+    End Function
+End Class
---
+++ Exercice17_3/ex17_3.vb
@@ -0,0 +1,50 @@
+﻿Module ex17_3
+
+    Sub Main()
+        Dim choix, element As String
+        Dim pile As New Pile()
+        Do
+            Console.WriteLine("1. Empiler")
+            Console.WriteLine("2. Dépiler")
+            Console.WriteLine("3. Tester si la Pile est vide")
+            Console.WriteLine("4. Nombre d'éléments dans la Pile")
+            Console.WriteLine("5. Contenu de la Pile")
+            Console.WriteLine("6. Quitter")
+            Console.WriteLine("Choix ?")
+            choix = Console.ReadLine()
+            Select Case choix
+                Case 1
+                    Console.WriteLine("Entrer l'élément à empiler.")
+                    element = Console.ReadLine()
+                    If pile.Empiler(element) Then
+                        Console.WriteLine("Element ajoutée avec succès.")
+                    Else
+                        Console.WriteLine("Une erreur est survenu.")
+                    End If
+                Case 2
+                    Dim depile As String = pile.Depiler()
+                    If depile IsNot Nothing Then
+                        Console.WriteLine("Valeur extraite de la pile : " + depile)
+                    Else
+                        Console.WriteLine("Aucune valeure n'a pu être dépilée.")
+                    End If
+                Case 3
+                    If pile.EstVide Then
+                        Console.WriteLine("La Pile est vide")
+                    Else
+                        Console.WriteLine("La Pile n'est pas vide")
+                    End If
+                Case 4
+                    Console.WriteLine("Nombre d'élément dans la pile : " + pile.NombreDElements.ToString)
+                Case 5
+                    Console.WriteLine(pile.ToString)
+                Case 6
+                    Console.WriteLine("Au revoir !")
+                    Console.ReadLine()
+                Case Else
+                    Console.WriteLine("Veuillez choisir un nombre entre 1 et et 6 compris.")
+            End Select
+        Loop Until choix = "6"
+    End Sub
+
+End Module
